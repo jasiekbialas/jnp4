79cea50ee107409da0d5c44308ebf3fc1c6c0629
Karol Soczewica <ks394468@students.mimuw.edu.pl>
2019-12-03 18:07:34 +0100

    z hashowaniem

--- fibin.cc
+++ fibin.cc
@@ -6,15 +6,20 @@ using namespace std;
 
 int main() {
 
-cout << Fib<19>::value() << endl;
+/*cout << Fib<19>::value() << endl;
 cout << Lit<Fib<10>>::value() << endl;
 cout << Lit<True>::value() << endl;
 cout << Lit<False>::value() << endl;
 cout << Inc1<Fib<19>>::value() << endl;
 cout << Inc1<Lit<False>>::value() << endl;
 cout << Inc10<Inc1<Lit<False>>>::value() << endl;
-cout << Eq<Lit<Fib<0>>, Lit<Fib<1>>>::value() << endl;
+cout << Eq<Lit<Fib<0>>, Lit<Fib<1>>>::value() << endl;*/
 cout << Fibin<int>::eval<Inc10<Inc1<Lit<Fib<10>>>>>() << endl;
 Fibin<const char*>::eval<Inc1<Lit<Fib<10>>>>();
 
+static_assert(1 == Fibin<uint8_t>::eval<If<Lit<False>, Lit<Fib<0>>, Lit<Fib<1>>>>());
+
+static_assert(59 == Fibin<uint64_t>::eval<Invoke<Lambda<Var<1>,
+            Sum<Ref<Var<1>>, Inc10<Lit<Fib<1>>>, Lit<Fib<2>>>>, Lit<Fib<3>>>>());
+static_assert(1 == Fibin<int16_t>::eval<Let<Var<3>, Lit<Fib<0>>, Inc1<Ref<Var<3>>>>>());
 }
--- fibin.h
+++ fibin.h
@@ -3,105 +3,182 @@
 
 #include <iostream>
 #include <type_traits>
+#include <typeinfo>
+#include <cmath>
 
 using namespace std;
 
-template <int N>
-class Fib {
-public:
-    constexpr static unsigned long long value() {
-        static_assert(N >= 0);
-        if (N == 0) {
-            return 0;
-        } else if (N == 1) {
-            return 1;
-        } else {
-            unsigned long long first = 0;
-            unsigned long long second = 1;
-            unsigned long long result = 1;
+const int max_id_len = 6;
+const int hash_const = 37;
 
-            for (int i = 0; i < N - 1; i++) {
-                result = first + second;
-                first = second;
-                second = result;
-            }
+struct True;
+struct False;
 
-            return result;
-        }
-    };
-};
+template<int N>
+struct Fib;
 
-class True {};
+template <typename T>
+struct Lit;
 
-class False {};
+template<typename ... Args>
+struct Sum;
 
-template <class T>
-class Lit;
+template <typename Var, typename Val, typename Ex>
+struct Let;
 
 template <int n>
-class Lit<Fib<n>> {
-public:
-    constexpr static unsigned long long value() {
-        return Fib<n>::value();
+struct Var;
+
+template <typename T>
+struct Ref;
+
+template <int n>
+struct Ref<Var<n>>;
+
+template <typename T>
+struct Inc1;
+
+template <typename T>
+struct Inc10;
+
+template <typename A, typename B>
+struct Lambda;
+
+template <typename A, typename B>
+struct Invoke;
+
+template<typename A, typename B, typename C>
+struct If;
+
+template<bool b, typename B, typename C>
+struct Choose;
+
+template<typename A, typename B>
+struct Eq;
+
+template<typename VT, typename T, typename ...R>
+struct Expr;
+
+constexpr static int hash_char(const char c) {
+    char x = c;
+    if (x >= 'A' && x <= 'Z') {
+        x -= 'A' + 1;
+    } else if (x >= 'a' && x <= 'z') {
+        x -= 'a' + 1;
+    } else if (x >= '0' && x <= '9') {
+        x -= '0';
+        x += ('z' - 'a') + 1;
+    } else {
+        x = 0;
     }
+
+    return x;
+}
+
+constexpr static unsigned long long hash_str(const char *str) {
+    int result = 0;
+    char c = ' ';
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        c = hash_char(str[i]);
+        if (c == -1 || i >= max_id_len) {
+            return 0;
+        }
+        result += hash_char(str[i]) * pow(hash_const, i);
+    }
+
+    return result;
+}
+
+template<typename VT, typename A, int n, typename B, typename ...R>
+struct Expr<VT, Invoke<Lambda<Var<n>, A>, B> ,R...> {
+    constexpr static VT val = Expr<VT, A, Var<n>, B, R...>::val;
 };
 
-template <>
-class Lit<True> {
-public:
-    constexpr static bool value() {
-        return true;
-    }
+template<typename VT, typename B, typename C, typename ...R>
+struct Expr<VT, Choose<true, B, C>, R...> {
+    constexpr static VT val = Expr<VT, B, R...>::val;
 };
 
-template <>
-class Lit<False> {
-public:
-    constexpr static bool value() {
-        return false;
-    }
+template<typename VT, typename B, typename C, typename ...R>
+struct Expr<VT, Choose<false, B, C>, R...> {
+    constexpr static VT val = Expr<VT, C, R...>::val;
 };
 
-template <class T>
-class Inc1 {
-public:
-    constexpr static unsigned long long value() {
-        return T::value() + Fib<1>::value();
-    }
+template<typename VT, typename A, typename B, typename C, typename ...R>
+struct Expr<VT, If<A, B, C>, R...> {
+    constexpr static VT val = Expr<VT, Choose<Expr<VT, A, R...>::val, B, C>, R...>::val;
 };
 
-template <class T>
-class Inc10 {
-public:
-    constexpr static unsigned long long value() {
-        return T::value() + Fib<10>::value();
-    }
+template<typename VT, typename A, typename B, typename ...R>
+struct Expr<VT, Eq<A, B>, R...>{
+    constexpr static bool val = Expr<VT, A, R...>::val == Expr<VT, B, R...>::val;
 };
 
-template <class T1, class T2>
-class Eq {
-public:
-    constexpr static bool value() {
-        return T1::value() == T2::value();
-    }
+template<typename VT, typename T, int n, typename V, typename ...R>
+struct Expr<VT, Let<Var<n>,  T, V>, R...> {
+    constexpr static VT val = Expr<VT, V, Var<n>, T, R...>::val;
 };
 
-template <class ValueType, class Enabled = void>
-class Fibin {
-public:
-    template <class T>
+template<typename VT, typename T, int n, typename ...R>
+struct Expr <VT, Ref<Var<n>>, Var<n>, T, R... >{
+    constexpr static VT val = Expr<VT, T>::val;
+};
+
+template <typename VT, typename ...R>
+struct Expr<VT, Lit<True>, R...> {
+    constexpr static bool val = true;
+};
+
+template <typename VT, typename ...R>
+struct Expr<VT, Lit<False>, R...> {
+    constexpr static bool val = false;
+};
+
+template<typename VT, typename ...R>
+struct Expr<VT, Lit<Fib<0>>, R...> {
+    constexpr static VT val = 0;
+};
+
+template<typename VT, typename ...R>
+struct Expr<VT, Lit<Fib<1>>, R...> {
+    constexpr static VT val = 1;
+};
+
+template<typename VT, int n, typename ...R>
+struct Expr<VT, Lit<Fib<n>>, R...> {
+    constexpr static VT val =
+            Expr<VT, Lit<Fib<n-1>>>::val +
+            Expr<VT, Lit<Fib<n-2>>>::val;
+};
+
+template<typename VT, typename ... T, typename ...R>
+struct Expr<VT, Sum<T...>, R...>{
+    constexpr static VT val = (Expr<VT, T, R...>::val + ...);
+};
+
+template<typename VT, typename T, typename  ...R>
+struct Expr<VT, Inc1<T>, R...>{
+    constexpr static VT val = Expr<VT, T, R...>::val + Expr<VT, Lit<Fib<1>> >::val;
+};
+
+template<typename VT, typename T, typename ...R>
+struct Expr<VT, Inc10<T>, R...>{
+    constexpr static VT val = Expr<VT, T, R...>::val + Expr<VT, Lit<Fib<10>>>::val;
+};
+
+template <class VT>
+struct Fibin {
+    template <typename T, typename U = VT, typename enable_if<!is_integral<U>::value, int>::type = 0>
     constexpr static void eval() {
-        cout << "Fibin doesn't support: PKc" << endl;
+        cout << "Fibin doesn't support: " << typeid(VT).name() << endl;
+    }
+
+    template <typename T, typename U = VT, typename enable_if<is_integral<U>::value, int>::type = 0>
+    constexpr static VT eval() {
+        return Expr<VT, T>::val;
     }
 };
 
-template <class ValueType>
-class Fibin<ValueType, typename enable_if<is_integral_v<ValueType>>::type> {
-public:
-    template <class T>
-    constexpr static ValueType eval() {
-        return (ValueType) T::value();
-    }
-};
 
 #endif /* JNP1_4_FIBIN_H */
